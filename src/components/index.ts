d6798295b5fd0fff43cc4741ef1af012c051f09d
Shawn <v-zhaod@microsoft.com>
2023-08-23 10:32:31 +0800

    to remove the dependency from bundle

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,3 @@
-export { default as Panel } from "./Panel/Panel";
+export {  Panel } from "./Panel/Panel";
 
-export { default as Button } from "./Button";
+// export { default as Button } from "./Button";
